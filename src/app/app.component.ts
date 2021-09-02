1e6b2216e95534b60ff3b956e6d917ea7a4ad760
swasdev4511 <swastiksupadhye@gmail.com>
2021-09-03 00:30:48 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = "folks , Let's lear Tap operator!";
-  nums$ = of(1, 2, 3, 4, 5, 6);
+  nums$ = of(1, 2, 3, 4, 5, 6, 7, 8);
+  transformedObservables: Observable<string>;
+  subscription: Subscription;
 
   ngOnInit(): void {
-    const subsrciption = this.nums$
-      .pipe(tap(r => console.log(' Log ', r)))
-      .subscribe(res => {
-        console.log(' After subscr. ', res);
-      });
+    this.transformedObservables = this.nums$.pipe(
+      tap(r => console.log(' Log ', r)),
+      map(r => ` Result : ${r * 3}`),
+      take(4)
+    );
+    this.subscription = this.transformedObservables.subscribe(res =>
+      console.log(res)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
