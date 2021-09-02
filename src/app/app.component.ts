d16dfbce81c05160984b5fb486884af1d1fdb239
swasdev4511 <swastiksupadhye@gmail.com>
2021-09-03 00:45:37 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, of, Subscription } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
+import { map, reduce, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -10,14 +10,17 @@ import { map, take, tap } from 'rxjs/operators';
 export class AppComponent implements OnInit, OnDestroy {
   title = "folks , Let's learn Tap operator!";
   nums$ = of(1, 2, 3, 4, 5, 6, 7, 8);
-  transformedObservables: Observable<string>;
+  transformedObservables: Observable<any>;
   subscription: Subscription;
 
   ngOnInit(): void {
     this.transformedObservables = this.nums$.pipe(
-      tap(r => console.log(' Log ', r)),
-      map(r => ` Result : ${r * 3}`),
-      take(4)
+      tap(r => console.log(' Log - I ', r)),
+      map(r => r * 3),
+      tap(r => console.log(' Log - II ', r)),
+      take(4),
+      reduce((acc, curr) => acc + curr, 0),
+      map(r => `Sum is ${r}`)
     );
     this.subscription = this.transformedObservables.subscribe(res =>
       console.log(res)
